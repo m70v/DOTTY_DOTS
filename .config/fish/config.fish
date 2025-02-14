e54d32582d6f8d4de2c5e0db884742e3d96400fd
m70v <149091288+m70v@users.noreply.github.com>
2025-02-14 20:16:01 +0400

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,7 +10,7 @@ function fish_greeting
     echo ""
 end
 #starship init fish | source
-set -x HYPRSHOT_DIR /home/m70v/Pictures/
+set -x HYPRSHOT_DIR ~/Pictures/Screenshots
 alias droidkill="waydroid session stop && sudo waydroid container stop"
 alias cord="/home/m70v/.config/hypr/scripts/cord.sh"
 alias cach="sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches"
